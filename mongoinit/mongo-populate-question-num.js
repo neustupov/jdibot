a2bc9462130d896ed0b11be7761dcca9400058bb
neustupov@yandex.ru <Ruslano4ka123>
2021-04-14 15:49:35 +0300

    Перенес проперти бота в .env, изменил скрипты инита базы

---
+++ mongoinit/mongo-populate-question-num.js
@@ -0,0 +1,158 @@
+db.questionnum.insert(
+    [{
+        "_id": {
+            "$oid": "6043c18104d72375dc4de117"
+        },
+        "seq": 100,
+        "_class": "org.neustupov.javadevinterviewbot.model.QuestionNum"
+    },{
+        "_id": {
+            "$oid": "6043c1a704d72375dc4de118"
+        },
+        "seq": 101,
+        "_class": "org.neustupov.javadevinterviewbot.model.QuestionNum"
+    },{
+        "_id": {
+            "$oid": "6043c1ba04d72375dc4de119"
+        },
+        "seq": 102,
+        "_class": "org.neustupov.javadevinterviewbot.model.QuestionNum"
+    },{
+        "_id": {
+            "$oid": "6043c1bb04d72375dc4de11a"
+        },
+        "seq": 103,
+        "_class": "org.neustupov.javadevinterviewbot.model.QuestionNum"
+    },{
+        "_id": {
+            "$oid": "6043c1bb04d72375dc4de11b"
+        },
+        "seq": 104,
+        "_class": "org.neustupov.javadevinterviewbot.model.QuestionNum"
+    },{
+        "_id": {
+            "$oid": "6043c1bb04d72375dc4de11c"
+        },
+        "seq": 105,
+        "_class": "org.neustupov.javadevinterviewbot.model.QuestionNum"
+    },{
+        "_id": {
+            "$oid": "6043c1bb04d72375dc4de11d"
+        },
+        "seq": 106,
+        "_class": "org.neustupov.javadevinterviewbot.model.QuestionNum"
+    },{
+        "_id": {
+            "$oid": "6043c1bb04d72375dc4de11e"
+        },
+        "seq": 107,
+        "_class": "org.neustupov.javadevinterviewbot.model.QuestionNum"
+    },{
+        "_id": {
+            "$oid": "6044d3ba4f9cbd5d826f6528"
+        },
+        "seq": 108,
+        "_class": "org.neustupov.javadevinterviewbot.model.QuestionNum"
+    },{
+        "_id": {
+            "$oid": "6044d4484f9cbd5d826f6529"
+        },
+        "seq": 109,
+        "_class": "org.neustupov.javadevinterviewbot.model.QuestionNum"
+    },{
+        "_id": {
+            "$oid": "6044d768ba5db560b4182f64"
+        },
+        "seq": 110,
+        "_class": "org.neustupov.javadevinterviewbot.model.QuestionNum"
+    },{
+        "_id": {
+            "$oid": "6044fad36265376eb51d1611"
+        },
+        "seq": 111,
+        "_class": "org.neustupov.javadevinterviewbot.model.QuestionNum"
+    },{
+        "_id": {
+            "$oid": "6044fb016265376eb51d1612"
+        },
+        "seq": 112,
+        "_class": "org.neustupov.javadevinterviewbot.model.QuestionNum"
+    },{
+        "_id": {
+            "$oid": "6044fb266265376eb51d1613"
+        },
+        "seq": 113,
+        "_class": "org.neustupov.javadevinterviewbot.model.QuestionNum"
+    },{
+        "_id": {
+            "$oid": "6044fb886265376eb51d1614"
+        },
+        "seq": 114,
+        "_class": "org.neustupov.javadevinterviewbot.model.QuestionNum"
+    },{
+        "_id": {
+            "$oid": "6044fba86265376eb51d1615"
+        },
+        "seq": 115,
+        "_class": "org.neustupov.javadevinterviewbot.model.QuestionNum"
+    },{
+        "_id": {
+            "$oid": "6044fbbc6265376eb51d1616"
+        },
+        "seq": 116,
+        "_class": "org.neustupov.javadevinterviewbot.model.QuestionNum"
+    },{
+        "_id": {
+            "$oid": "6044fbd96265376eb51d1617"
+        },
+        "seq": 117,
+        "_class": "org.neustupov.javadevinterviewbot.model.QuestionNum"
+    },{
+        "_id": {
+            "$oid": "6044fd0a6265376eb51d1618"
+        },
+        "seq": 118,
+        "_class": "org.neustupov.javadevinterviewbot.model.QuestionNum"
+    },{
+        "_id": {
+            "$oid": "6044fd6e6265376eb51d1619"
+        },
+        "seq": 119,
+        "_class": "org.neustupov.javadevinterviewbot.model.QuestionNum"
+    },{
+        "_id": {
+            "$oid": "6050492c22e6e147f6e4445b"
+        },
+        "seq": 120,
+        "_class": "org.neustupov.javadevinterviewbot.model.QuestionNum"
+    },{
+        "_id": {
+            "$oid": "6050493022e6e147f6e4445c"
+        },
+        "seq": 121,
+        "_class": "org.neustupov.javadevinterviewbot.model.QuestionNum"
+    },{
+        "_id": {
+            "$oid": "6065db0d3ebdda1da66e654d"
+        },
+        "seq": 122,
+        "_class": "org.neustupov.javadevinterviewbot.model.QuestionNum"
+    },{
+        "_id": {
+            "$oid": "6065db0d3ebdda1da66e654e"
+        },
+        "seq": 123,
+        "_class": "org.neustupov.javadevinterviewbot.model.QuestionNum"
+    },{
+        "_id": {
+            "$oid": "6065db0d3ebdda1da66e654f"
+        },
+        "seq": 124,
+        "_class": "org.neustupov.javadevinterviewbot.model.QuestionNum"
+    },{
+        "_id": {
+            "$oid": "6065db0d3ebdda1da66e6550"
+        },
+        "seq": 125,
+        "_class": "org.neustupov.javadevinterviewbot.model.QuestionNum"
+    }]);
